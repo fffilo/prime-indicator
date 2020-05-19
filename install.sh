3308f8212c71799d56720884c3762997f29e3fb4
fffilo <fffilo666@gmail.com>
2020-05-19 10:55:43 +0200

    Install branch argument

--- install.sh
+++ install.sh
@@ -2,19 +2,23 @@
 
 URL="https://github.com/fffilo"
 UUID="prime-indicator"
+BRANCH="master"
 BUILD="/tmp"
+
+# first argument (branch) exists?
+[ -n "$1" ] && BRANCH="$1"
 
 # clear
 rm -rf ${BUILD}/${UUID}*
 
 # download
-wget -O ${BUILD}/${UUID}.zip ${URL}/${UUID}/archive/master.zip
+wget -O ${BUILD}/${UUID}.zip ${URL}/${UUID}/archive/${BRANCH}.zip
 
 # unarchive
 unzip ${BUILD}/${UUID}.zip -d ${BUILD}
 
 # install
-make --directory=${BUILD}/${UUID}-master install
+make --directory=${BUILD}/${UUID}-${BRANCH} install
 
 # clear
 rm -rf ${BUILD}/${UUID}*
